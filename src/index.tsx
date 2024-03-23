abb426c810b87ead294c63182a08652fc24e0459
Soheima Maradiaga <soheimam@gmail.com>
2024-03-23 19:53:59 +0100

    added gpt

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import {
   Button,
-  FarcasterUserDetailsInput,
-  FarcasterUserDetailsOutput,
   Frog,
-  getFarcasterUserDetails
+  getFarcasterUserDetails,
 } from "@airstack/frog";
-import crypto from 'crypto';
 import { config } from "dotenv";
 import { devtools } from 'frog/dev';
 import { serveStatic } from 'frog/serve-static';
-import fs from 'fs';
-import OpenAI from 'openai';
-import sharp from 'sharp';
+import { OpenAI, } from 'openai';
 const openAi = new OpenAI({
   apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
 });
@@ -23,77 +18,99 @@ export const app = new Frog({
   apiKey: process.env.AIRSTACK_API_KEY as string,
 });
 
-const convertJpegToPng = async (url: string) => {
-  // For some reason, airstack store images as jpeg, so we need to convert it to png
-  const response = await fetch(url);
-  console.log(`Response status: ${response.status}`)
-  if (!response.ok)
-    throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
-  // create md5 of filename using crypto
-  const md5 = crypto.createHash('md5').update(url).digest('hex');
-  const outputPath = `/tmp/${md5}.png`;
-  const arrayBuffer = await response.arrayBuffer();
-  const jpegBuffer = Buffer.from(arrayBuffer);
-  console.log(`Buffer length: ${jpegBuffer.length}`);
+//extract labels
+const extractLabelsFromImage = async (imageUrl: string) => {
+  const response = await openAi.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "user",
 
-  const pngBuffer = await sharp(jpegBuffer).toColourspace('srgb').ensureAlpha().toBuffer();
-  console.log(`PNG buffer length: ${pngBuffer.length}`);
-  fs.writeFileSync(outputPath, pngBuffer);
-  // Save the PNG buffer to a file
+        content: [
+          {
+            type: "text", text: "Based on the image, generate a description of a person with the following format: " +
+              "age [years old], sex [male/female/other], with: a [face size] face, a [nose size] nose, " +
+              "[eyes color] eyes, [hair color] [hair style] hair, [mouth size] mouth, [stature], [body size]. " +
+              "Describe the background as [background]. Keep the description no more than 700 chars."
+          },
+          {
+            type: "image_url",
+            image_url: {
+              "url": imageUrl,
+            },
+          },
+        ],
+      },
+    ],
+  });
 
-  return fs.createReadStream(outputPath);
+  return response.choices?.[0].message.content;
+}
+//convert and edit image
+async function convertAndEditImage(_prompt: string) {
+  try {
+    const Originalprompt = `Create a single chibi-style character, centered in the composition, from the photo provided. The image should be a close-up, front shot where the character looks directly at the viewer with large, expressive eyes and a wide, happy smile. Focus on making the head and shoulders prominent, embodying the charm of chibi art. Use a plain, colorful background that complements the character without any other figures, text, or distractions. The overall effect should be playful and endearing, highlighting the character's joyful persona." ${_prompt} `
+
+    const prompt = `DO NOT add any detail, just use it AS-IS: ${Originalprompt}`;
+
+    // Use the image stream directly for the OpenAI call
+    const openAiResponse = await openAi.images.generate({
+      // image: imageStream,
+      model: "dall-e-3",
+      response_format: 'url',
+      style: "vivid",
+      size: "1024x1024",
+      prompt: prompt,
+    });
+
+    console.log(openAiResponse, 'OpenAI Response');
+    return openAiResponse.data[0].url
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
-app.frame("/", async (c) => {
-  const { status } = c;
-  const input: FarcasterUserDetailsInput = {
-    fid: 602,
-  };
-  const { data, error }: FarcasterUserDetailsOutput =
-    await getFarcasterUserDetails(input);
-
-  // We need to download the image from the URL above and send it to OpenAI
-  // Either via a stream or a buffer
-  const url = data?.profileImage?.original
-  console.log(url)
-  if (!url) throw new Error('No image found');
-
-  // Stream the image from the URL into a buffer
-  const response = await fetch(url);
-  if (!response.ok) throw new Error('Failed to fetch image');
-  const stream = await convertJpegToPng(url);
-  // console.log(`The Stream: ${stream}`)
-  // const uploadResponse = await openAi.files.create({
-  //   purpose: "fine-tune",
-  //   file: stream,
-  // });
-  // const imageId = uploadResponse.id
-
-  const variant = await openAi.images.edit({
-    prompt: "Turn this image into The Simpsons theme and colors, ensure its true to the simpsons universe and style",
-    image: './scarlett-johansson.png',
-    n: 1,
-    model: "dall-e-2",
-    size: "256x256",
-    response_format: "url"
-  })
-  console.log(variant)
-
-  if (error) throw new Error(error);
-
-  console.log(data, 'farcaster user details');
+app.frame('/', (c) => {
   return c.res({
+    action: '/submit',
     image: (
-      <div
-        style={{
-          color: "green",
-          display: "flex",
-          fontSize: 40,
-        }}
-      >
-        {status === "initial" ? "Initial Frame" : "Response Frame"}
+      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+        Click to cuteify your image
       </div>
     ),
+    intents: [
+      <Button value="start">Start</Button>,
+    ]
+  })
+})
+
+
+app.frame("/submit", async (c) => {
+  const { status } = c;
+  console.log(c, 'status');
+  const input = {
+    fid: c.frameData?.fid || 1
+  };
+
+  const { data, error } = await getFarcasterUserDetails(input);
+  if (error) {
+    console.error(error);
+    // Since we're avoiding try/catch, make sure to handle the error appropriately here.
+    return;
+  }
+
+  if (!data?.profileImage?.original) {
+    return
+  }
+  const labels = await extractLabelsFromImage(data?.profileImage?.original);
+  if (!labels) {
+    return;
+  }
+  const output = await convertAndEditImage(labels);
+  console.log(output);
+
+  return c.res({
+    image: output as string,
     intents: [status === "initial" && <Button>Click Here</Button>],
   });
 });
